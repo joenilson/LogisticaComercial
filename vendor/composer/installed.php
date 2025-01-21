88244c48e383ac928ecaafd19d0d6aaa691edca0
Joe Nilson <joenilson@gmail.com>
2025-01-21 12:54:34 -0400

    First push with composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'joenilson/logisticacomercial',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c6169e642819cbb8e3fa7296a0a9751f52523dcd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'joenilson/logisticacomercial' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c6169e642819cbb8e3fa7296a0a9751f52523dcd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
